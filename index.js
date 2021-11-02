a125058f50b24730309565e160a26799a3497991
niamzzif <henrywang3@g.ucla.edu>
2021-11-01 21:20:14 -0700

    connecting backend to frontend

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
+app.get("/test", (req, res) => {
+  res.send("TEST");
+});
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`);
 });
