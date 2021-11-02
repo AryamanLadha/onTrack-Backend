98b28446e9430a6ed677d6dd77c894f0db5cff7c
Dylan Zhang <dylan.zhang@gmail.com>
2021-11-01 17:48:15 -0700

    setup mongoDB database and connection

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+require('dotenv').config();
+
+// connect to database
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URI)
+  .then(() => console.log(`Database connected successfully`))
+  .catch(err => console.log(err));
+
 const express = require('express')
 const app = express();
 const port = 8000;
