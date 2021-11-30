4b8e1c220758b109405d6faba1c5acc83f86a40f
jason2020 <jtay20@punahou.edu>
2021-11-29 19:31:07 -0800

    Completed eligible class function with dummy valid class data

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -1,4 +1,3 @@
-import mongoose from "mongoose";
 import Classes from "./../models/Classes.js"
 import DetailedClass from "./../models/DetailedClass.js"
 
@@ -13,7 +12,73 @@ controller.getSingle = async (req, res) => {
 }
 
 controller.getEligible = async (req, res) => {
-    res.send("NOT IMPLEMENTED: get eligible courses");
+    // TODO: Get quarter instead of using this hardcoded value
+    const currQuarter = "Winter 2021"
+    let data = {};
+    try {
+        data = JSON.parse(req.query.studentData);
+    } catch {
+        res.status(400).send("Invalid JSON");
+    }
+
+    if (!data.hasOwnProperty("completedClasses") || !data.hasOwnProperty("currentClasses")) {
+        res.status(400).send("Invalid JSON");
+    }
+
+    // TODO: get valid courses for major (using fake data for now)
+    const avaliableClasses = {
+        "Computer Science": ["COM SCI 180", "COM SCI 181", "COM SCI 31", "COM SCI 32", "COM SCI 181"]
+    }
+
+    const completedClasses = data.completedClasses.length > 0 ? data.completedClasses.reduce((a, v) => ({ ...a, [v]: 0 }), {}) : {};
+    console.log(completedClasses);
+    const currentClasses = data.currentClasses.length > 0 ? data.currentClasses.reduce((a, v) => ({ ...a, [v]: 0 }), {}) : {};
+    console.log(currentClasses);
+
+    const eligibleClasses = [{ "quarter": currQuarter, "subjects": {} }];
+    for (const subjects in avaliableClasses) {
+        for (const course in avaliableClasses[subjects]) {
+            let currCourse = await DetailedClass.byName(avaliableClasses[subjects][course]);
+            // Class not found in database (invalid short name or missing from database)
+            if (currCourse.length === 0) {
+                // TODO: Handle this error. This is quite bad
+                console.log("NOT FOUND", avaliableClasses[subjects][course]);
+            } else {
+                let addCourse = true;
+                // Check if class is already completed or in progress
+                if (completedClasses.hasOwnProperty(currCourse[0]["Name"] || currentClasses.hasOwnProperty(currCourse[0]["Name"]))) {
+                    addCourse = false;
+                }
+                for (const course in currCourse[0]["Enforced Prerequisites"]) {
+                    if (!addCourse) {
+                        break;
+                    }
+                    if (!completedClasses.hasOwnProperty([currCourse[0]["Enforced Prerequisites"][course]])) {
+                        addCourse = false;
+                        break;
+                    }
+                }
+                for (const course in currCourse[0]["Enforced Corequisites"]) {
+                    if (!addCourse) {
+                        break;
+                    }
+                    if (!currentClasses.hasOwnProperty([currCourse[0]["Enforced Corequisites"][course]])) {
+                        addCourse = false;
+                        break;
+                    }
+                }
+
+                if (addCourse) {
+                    if (eligibleClasses[0].subjects.hasOwnProperty(subjects)) {
+                        eligibleClasses[0].subjects[subjects].push(currCourse[0]["Name"]);
+                    } else {
+                        eligibleClasses[0].subjects[subjects] = [currCourse[0]["Name"]];
+                    }
+                }
+            }
+        }
+    }
+    res.send(JSON.stringify(eligibleClasses));
 }
 
 export default controller;
