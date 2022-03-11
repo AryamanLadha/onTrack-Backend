29b6f362145019c88dfdbfa70eab6b31693b6e87
jason2020 <jtay20@punahou.edu>
2022-03-11 00:45:13 -0800

    Experimenting with manually setting cookies

--- index.js
+++ index.js
@@ -54,15 +54,33 @@ app.options("*", cors(corsConfig));
 // );
 
 app.set("trust proxy", 1); // trust first proxy
-app.use(
-  cookieSession({
-    name: "session-name",
-    keys: [
-      process.env.COOKIE_SESSION_SECRET1,
-      process.env.COOKIE_SESSION_SECRET2,
-    ],
-  })
-);
+// app.use(
+//   cookieSession({
+//     name: "session-name",
+//     keys: [
+//       process.env.COOKIE_SESSION_SECRET1,
+//       process.env.COOKIE_SESSION_SECRET2,
+//     ],
+//   })
+// );
+
+
+// set a cookie
+app.use(function (req, res, next) {
+  // check if client sent cookie
+  var cookie = req.cookies.cookieName;
+  if (cookie === undefined) {
+    // no: set a new cookie
+    var randomNumber = Math.random().toString();
+    randomNumber=randomNumber.substring(2,randomNumber.length);
+    res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
+    console.log('cookie created successfully');
+  } else {
+    // yes, cookie was already present 
+    console.log('cookie exists', cookie);
+  } 
+  next(); // <-- important!
+});
 
 // app.use(session({ 
 //   secret: process.env.EXPRESS_SECRET, 
